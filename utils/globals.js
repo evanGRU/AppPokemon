654077b9cf97c37bddfa78990741c369a1e84d15
evanGRU <evangruchot.pro@gmail.com>
2024-01-31 08:52:49 +0100

    create global file with all the title and names

---
+++ utils/globals.js
@@ -0,0 +1,10 @@
+export const globals = {
+    homeTabName: "HomeTab",
+    searchTabName: "SearchTab",
+    teamTabName: "TeamTab",
+    paramsTabName: "SettingsTab",
+    homeStackName: "HomeStack",
+    detailStackName: "DetailsStack",
+    homeTitle: "Pokédex",
+    detailTitle: "Fiche Pokédex"
+}
