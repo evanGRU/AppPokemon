2da3ab5607ca820edb97beb6012edaee02b78907
evanGRU <evangruchot.pro@gmail.com>
2024-01-31 08:51:50 +0100

    organize app.js by adding Tabs and Stacks

---
+++ views/TeamScreen.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export default function TeamScreen() {
+
+    return null;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
